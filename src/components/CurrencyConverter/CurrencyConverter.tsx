31385fd9caf572982eb1c1d6d6c943cccd9a8b23
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-12-01 18:06:55 +0200

    feat: styles for CurrencyConverter

--- src/components/CurrencyConverter/CurrencyConverter.tsx
+++ src/components/CurrencyConverter/CurrencyConverter.tsx
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { BASE_URL } from "../../constants/index";
+import { API_KEY, BASE_URL } from "../../constants/index";
+import styles from "./CurrencyConverter.module.scss";
 
 interface ApiResponse {
   rates: {};
 }
-
-const apiKey = process.env.REACT_APP_FIXER_API_KEY;
 
 const CurrencyConverter: React.FC = () => {
   const [amount1, setAmount1] = useState<number>(1);
   const [amount2, setAmount2] = useState<number>(1);
   const [currency1, setCurrency1] = useState<string>("UAH");
   const [currency2, setCurrency2] = useState<string>("UAH");
-  const [currencies, setCurrencies] = useState<string[]>([]);
+  const [currencies, setCurrencies] = useState({});
   const [rates, setRates] = useState<{ [key: string]: number }>({});
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get<ApiResponse>(
-          `${BASE_URL}?apikey=${apiKey}`
+          `${BASE_URL}/latest?apikey=${API_KEY}`
         );
         setRates(response.data.rates);
-        setCurrencies(Object.keys(response.data.rates));
+        const currencies = await axios.get(
+          `${BASE_URL}/symbols?apikey=${API_KEY}`
+        );
+
+        setCurrencies(currencies.data.symbols);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -37,13 +40,14 @@ const CurrencyConverter: React.FC = () => {
     currencyNumber: number
   ) => {
     const selectedCurrency = event.target.value;
+
     if (currencyNumber === 1) {
       setCurrency1(selectedCurrency);
       const newAmount = (amount1 * rates[currency2]) / rates[selectedCurrency];
       setAmount2(parseFloat(newAmount.toFixed(3)));
     } else {
       setCurrency2(selectedCurrency);
-      const newAmount = (amount1 * rates[currency2]) / rates[selectedCurrency];
+      const newAmount = (amount1 * rates[selectedCurrency]) / rates[currency1];
       setAmount2(parseFloat(newAmount.toFixed(3)));
     }
   };
@@ -64,32 +68,47 @@ const CurrencyConverter: React.FC = () => {
   };
 
   return (
-    <div>
-      <div>
+    <div className={styles.converterContainer}>
+      <h1 className={styles.title}> Currency Converter</h1>
+      <div className={styles.currencyContainer}>
         <input
           type="number"
           value={amount1}
           name="amount1"
           onChange={(e) => handleChange(e)}
           placeholder="Enter Amount"
+          className={styles.amountInput}
         />
-        <select value={currency1} onChange={(e) => handleCurrencyChange(e, 1)}>
-          {currencies.map((currency) => (
-            <option value={currency}>{currency}</option>
+        <select
+          value={currency1}
+          onChange={(e) => handleCurrencyChange(e, 1)}
+          className={styles.currencySelect}
+        >
+          {Object.entries(currencies).map(([abbreviation, description]) => (
+            <option key={abbreviation} value={abbreviation}>
+              {`${abbreviation} - ${description}`}
+            </option>
           ))}
         </select>
       </div>
-      <div>
+      <div className={styles.currencyContainer}>
         <input
           type="number"
           value={amount2}
           name="amount2"
           onChange={(e) => handleChange(e)}
           placeholder="Enter Amount"
+          className={styles.amountInput}
         />
-        <select value={currency2} onChange={(e) => handleCurrencyChange(e, 2)}>
-          {currencies.map((currency) => (
-            <option value={currency}>{currency}</option>
+        <select
+          value={currency2}
+          onChange={(e) => handleCurrencyChange(e, 2)}
+          className={styles.currencySelect}
+        >
+          {Object.entries(currencies).map(([abbreviation, description]) => (
+            <option key={abbreviation} value={abbreviation}>
+              {`${abbreviation} - ${description}`}
+            </option>
           ))}
         </select>
       </div>
